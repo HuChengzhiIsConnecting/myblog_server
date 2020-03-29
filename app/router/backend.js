e7227f3c3f254f58f1832d53c0864f6799d0929e
huchengzhi <297799265@qq.com>
2020-03-29 22:11:23 +0800

    新增文章功能基本实现

--- app/router/backend.js
+++ app/router/backend.js
@@ -2,10 +2,11 @@ module.exports = app => {
   const auth = app.middleware.auth()
   
   app.router.post('/api/b/login', app.controller.login.login)//后台用户登录
-  app.router.get('/api/b/articleList', auth, app.controller.backend.articleList)//后台获取文章列表
+  app.router.post('/api/b/createArticle', app.controller.backend.createArticle)//新建文章
+  app.router.get('/api/b/articleList', app.controller.backend.articleList)//后台获取文章列表
   app.router.get('/api/b/cateList', app.controller.backend.cateList)//后台获取分类列表
   app.router.post('/api/b/createCate', app.controller.backend.createCate)//新增分类
   app.router.post('/api/b/createTag', app.controller.backend.createTag)//新增标签
   app.router.get('/api/b/tagList', app.controller.backend.tagList)//后台获取标签列表
-  app.router.get('/api/b/getQiniuToken', auth, app.controller.backend.getQiniuToken)  // 获取七牛token
+  app.router.get('/api/b/getQiniuToken', app.controller.backend.getQiniuToken)  // 获取七牛token
 };
