a9dc94bd5c2c591dc3bea805a2bed124157b5cd7
huchengzhi <297799265@qq.com>
2020-03-31 20:24:11 +0800

    新增前台文章列表接口

--- app/controller/frontend.js
+++ app/controller/frontend.js
@@ -1,11 +1,75 @@
 'use strict';
-
+const moment = require('moment')
 const Controller = require('egg').Controller;
 
 class FrontendController extends Controller {
   async blogList() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
+  }
+  async articleListByDate() {
+    const { ctx } = this;
+    let mapData = new Map();
+    let res = await ctx.service.frontend.articleListByDate(ctx.query);
+    // console.log(res);
+    if (!res.length) {
+      return ctx.body = {
+        code: 201,
+        msg: "文章为空，请先添加文章",
+        data: []
+      }
+    }
+    res.forEach((item) => {
+      let date = moment(item.updated_at).format("YYYY-MM-DD HH:mm:ss");
+      let year = date.substring(0, 4);
+      // console.log(year)
+      // console.log(mapData.has(year))
+      if (!mapData.has(year)) {
+        mapData.set(year, [item]);
+      } else {
+        mapData.set(year, [...mapData.get(year), item]);
+      }
+    })
+    ctx.body = {code:200,msg:'操作成功',data:[...mapData]};
+  }
+  async articleDetail(){
+    const { ctx } = this;
+    let res = await ctx.service.frontend.articleDetail(ctx.query);
+    let cateRes="";
+    let tagsRes=[];
+    console.log(res);
+    if(!res){
+      return ctx.body={
+        code:201,
+        msg:"文章不存在",
+        data:{}
+      }
+    }
+    let data={id:res.id,title:res.title,content:res.content,updated_at:res.updated_at,view_num:res.view_num};
+    let cate_id=res.cate_id;
+    let tags_group=!!res.tags_group && res.tags_group.split(',');
+    if(!!cate_id){
+      //存在分类的话查询
+      cateRes = await ctx.service.frontend.findCateName(cate_id);
+      console.log("cateres:",cateRes)
+      data['cate_name']=cateRes.name;
+    }
+    if(tags_group){
+      //存在标签的话
+      tags_group=tags_group.map((item)=>{
+        return parseInt(item)
+      })
+      tagsRes = await ctx.service.frontend.findTagsName(tags_group);
+      console.log("tags:",tagsRes);
+      data['tags_name']=tagsRes.map((item)=>{
+        return item.name
+      });
+    }
+    ctx.body={
+      code:200,
+      msg:'success',
+      data:data
+    };
   }
 }
 
---
+++ app/service/frontend.js
@@ -0,0 +1,52 @@
+const Service = require('egg').Service;
+const qiniu = require('qiniu')
+class FrontendService extends Service {
+  async articleListByDate() {
+    const { ctx } = this
+    let result = await ctx.model.Article.findAll({
+      where: {
+        status: 1
+      },
+      order: [
+        ['updated_at', 'desc']
+      ],
+      attributes: ['id', 'title','updated_at']
+    });
+   return result;
+  }
+  async articleDetail({id}) {
+    const { ctx } = this
+    let result = await ctx.model.Article.findOne({
+      where: {
+        id: id,
+        status:1
+      },
+      attributes: ['id', 'title','content','cate_id','tags_group','updated_at','view_num']
+    });
+   return result;
+  }
+  async findCateName(cate_id) {
+      //查询文章分类名称
+    const { ctx } = this
+    let result = await ctx.model.Cate.findOne({
+      where: {
+        id: cate_id,
+        status:1
+      },
+      attributes: ['id', 'name']
+    });
+   return result;
+  }
+  async findTagsName(tags_group) {
+    const { ctx } = this
+    let result = await ctx.model.Tag.findAll({
+      where: {
+        id: tags_group,
+        status:1
+      },
+      attributes: ['name']
+    });
+   return result;
+  }
+}
+module.exports = FrontendService
